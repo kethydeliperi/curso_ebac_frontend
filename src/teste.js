791d1871596cf505adf44d4158e2fe2bb26ecffa
Kethyllen Deliperi <kethyllenldeliperi@gmail.com>
2024-01-23 13:37:05 -0300

    Enviando exercício de typescript

---
+++ src/teste.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicar = (x, y) => x * y;
+const resultado = multiplicar(5, 3);
+console.log(resultado);
+const saudacao = (nome) => "Olá " + nome;
+const mensagemSaudacao = saudacao("João");
+console.log(mensagemSaudacao);
---
+++ src/teste.ts
@@ -0,0 +1,10 @@
+const multiplicar = (x:number, y:number):number => x * y
+
+const resultado = multiplicar(5, 3);
+console.log(resultado);
+  
+const saudacao = (nome: string): string => "Olá " + nome
+
+  const mensagemSaudacao = saudacao("João");
+  console.log(mensagemSaudacao);
+  
